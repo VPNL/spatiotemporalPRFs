ce53f36d7ecbdb1a1b24fc1f03867b713c85099d
Insub Kim <insekim@gmail.com>
2021-09-07 16:48:05 -0700

    getSTStimulus added

--- getSTStimulus.m
+++ getSTStimulus.m
@@ -5,18 +5,19 @@
 % input:
 % params.stim.images_unconvolved: This is the output of the mrVista rmMakeStimulus.m function
 % params.stim.instimwindow: Grab pixels where stimulus was actully presented
+% this is to only compute pixels where the stimulus was presented (saves computational resources)
 % stimulusNumber: index of the stimulus that is being grabbed
 
 % Return values:
-% stim:
-% keep: to only compute pixels where the stimulus was presented (saves computational resources)
+% stim: [Time(ms) X pixels]
+% keep: [pixels]
 
 % note: for more information check rmMakeStimulus.m
 
 function [stim,keep] = getSTStimulus(params,stimulusNumber)
 
 % get stimulus and keep values
-stim = params.stim(stimulusNumber).images_unconvolved';
+stim = params.stim(stimulusNumber).images_unconvolved'; 
 keep = params.stim.instimwindow;
 
 
