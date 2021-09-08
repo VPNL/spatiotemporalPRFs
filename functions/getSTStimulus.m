78e271566e1b9f77cf36530043ebe8d0b46b5b7f
Eline Kupers <elinekupers@gmail.com>
2021-09-08 14:06:00 -0700

    define sparsify flag of stimulus pixels here if undefined

--- functions/getSTStimulus.m
+++ functions/getSTStimulus.m
@@ -1,14 +1,14 @@
-function [stim,keep] = getSTStimulus(params,stimulusNumber)
+function [stim,keepPixels] = getSTStimulus(params,stimulusNumber)
 %% Function to get spatiotemporal stimulus images and parameters
 %
-%   [stim,keep] = getSTStimulus(params,stimulusNumber)
+%   [stim,keepPixels] = getSTStimulus(params,stimulusNumber)
 %
 % Grabs stimulus and params for each stimulus file
 %
 % INPUT: 
 % params.stim.images_unconvolved :   Aperture images [x,y,t]. Can be made
 %                                    by mrVista rmMakeStimulus.m function
-% params.stim.instimwindow       :   Pixels where stimulus was actually
+% [params.stim.instimwindow]     :   Pixels where stimulus was actually
 %                                    presented. This window is used to 
 %                                    save computational resources when 
 %                                    generating predictions given the
@@ -23,7 +23,11 @@ function [stim,keep] = getSTStimulus(params,stimulusNumber)
 % keep values
 
 stim = params.stim(stimulusNumber).images_unconvolved; % [EK]: why transpose if we can just input the correct dimension? 
-keep = params.stim.instimwindow;
 
+if params.stim.sparsifyFlag
+    keepPixels = params.stim.instimwindow;
+else
+    keepPixels = true(1,size(stim,1));
+end
 
 end
