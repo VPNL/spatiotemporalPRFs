f76f01cc597ff0c680083b3347c419d5f23c91ab
Eline Kupers <elinekupers@gmail.com>
2021-10-13 15:27:39 -0700

    change names of in and outputs to keep overview

--- getPredictedBOLDResponse.m
+++ getPredictedBOLDResponse.m
@@ -4,12 +4,11 @@ function predBOLD = getPredictedBOLDResponse(params, predNeural, hrf)
 
 for n = 1:size(predNeural,2)
     chanResponse = num2cell(predNeural{n},1)';
-    
     cellhrf = repmat({hrf}, size(chanResponse,1), 1);
-    predBOLD = cellfun(@(X, Y) convolve_vecs(X, Y, params.analysis.temporal.fs, 1 /params.analysis.temporal.tr), ...
+    predBOLD1 = cellfun(@(X, Y) convolve_vecs(X, Y, params.analysis.temporal.fs, 1 /params.analysis.temporal.tr), ...
             chanResponse, cellhrf, 'uni', false);
-    predBOLD = cellfun(@transpose,predBOLD,'UniformOutput',false);
-    predBOLD = cell2mat(predBOLD)';
+    predBOLD2 = cellfun(@transpose,predBOLD1,'UniformOutput',false);
+    predBOLD{n} = cell2mat(predBOLD2)';
     
 end
 
