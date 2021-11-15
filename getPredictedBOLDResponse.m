13d60cd7e459ef2ccb9c0a7ff95b5177d77ba7ed
Eline Kupers <elinekupers@gmail.com>
2021-11-15 09:55:00 -0800

    transpose if needed

--- getPredictedBOLDResponse.m
+++ getPredictedBOLDResponse.m
@@ -3,6 +3,9 @@ function predBOLD = getPredictedBOLDResponse(params, predNeural, hrf)
 % for each voxel
 
 for n = 1:size(predNeural,2)
+    if size(predNeural{n},1) < size(predNeural{n},2)
+        predNeural{n} = predNeural{n}';
+    end
     chanResponse = num2cell(predNeural{n},1)';
     cellhrf = repmat({hrf}, size(chanResponse,1), 1);
     predBOLD1 = cellfun(@(X, Y) convolve_vecs(X, Y, params.analysis.temporal.fs, 1 /params.analysis.temporal.tr), ...
