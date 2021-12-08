3a9e9f7c31964c3f36df9a56622030ff8666d69c
Eline Kupers <elinekupers@gmail.com>
2021-12-07 16:14:14 -0800

    first save to struct p to avoid overwriting, also avoid overwriting exponent in case we want to use its median per ROI

--- functions/getTemporalParams.m
+++ functions/getTemporalParams.m
@@ -47,52 +47,55 @@ else % load default temporal params if Constant file is not there
     tr = 1; % sec
     switch params.analysis.temporalModel
         case '1ch-dcts' % load 1ch-dcts (DN) model params
-            params.num_channels = 1;
-            params.fs           = 1000; % sample rate (Hz)
-            params.tau1         = 0.05; % time constant for initial gamma IRF
-            params.weight       = 0;    % positive vs negative IRF weight (0 means basically no negative IRF)
-            params.tau2         = 0.1;  % time constant for delayed normalization
-            params.n            = 2;    % exponent
-            params.sigma        = 0.1;  % constant to avoid dividing by zero
-            params.shift        = 0;    % shift onset of start response (in ms?)
-            params.scale        = 1;    % scale factor of final neural response
+            p.num_channels = 1;
+            p.fs           = 1000; % sample rate (Hz)
+            p.tau1         = 0.05; % time constant for initial gamma IRF
+            p.weight       = 0;    % positive vs negative IRF weight (0 means basically no negative IRF)
+            p.tau2         = 0.1;  % time constant for delayed normalization
+            p.n            = 2;    % exponent
+            p.sigma        = 0.1;  % constant to avoid dividing by zero
+            p.shift        = 0;    % shift onset of start response (in ms?)
+            p.scale        = 1;    % scale factor of final neural response
             
         case '2ch-exp-sig' % load 2ch-exp-sig (2ch) model params
-            params.num_channels = 2;
-            params.fs           = 1000;  % sample rate (Hz)
-            params.tau_s        = 4.93;  % time constant for excitatory mechanism (ms) to create sustained gamma IRF
-            params.n1           = 9;     % number of stages in excitatory mechanism
-            params.n2           = 10;    % number of stages in inhibitory mechanism
-            params.kappa        = 1.33;  % ratio of time constants for primary/secondary filters
-            params.tau_ae       = 10000; % time constant for adaptive exponential decay (ms)
-            params.Lp           = 0.1;   % Sigmoid Lambda for positive part of transient response (i.e. mid saturation point)
-            params.Kp           = 3;     % Sigmoid Kappa for positive part of transient response (i.e. the slope)
-            params.Kn           = 3;     % Sigmoid Kappa for negative part of transient response (i.e. the slope)
-            params.weight       = 0.5;   % relative weight sustained vs transient (0.5 = equal weight for either channel)
-            params.shift        = 0;     % shift of onset response (in ms?)
+            p.num_channels = 2;
+            p.fs           = 1000;  % sample rate (Hz)
+            p.tau_s        = 4.93;  % time constant for excitatory mechanism (ms) to create sustained gamma IRF
+            p.n1           = 9;     % number of stages in excitatory mechanism
+            p.n2           = 10;    % number of stages in inhibitory mechanism
+            p.kappa        = 1.33;  % ratio of time constants for primary/secondary filters
+            p.tau_ae       = 10000; % time constant for adaptive exponential decay (ms)
+            p.Lp           = 0.1;   % Sigmoid Lambda for positive part of transient response (i.e. mid saturation point)
+            p.Kp           = 3;     % Sigmoid Kappa for positive part of transient response (i.e. the slope)
+            p.Kn           = 3;     % Sigmoid Kappa for negative part of transient response (i.e. the slope)
+            p.weight       = 0.5;   % relative weight sustained vs transient (0.5 = equal weight for either channel)
+            p.shift        = 0;     % shift of onset response (in ms?)
             
         case '1ch-glm' % load 1ch-glm (linear) model params
-            params.num_channels = 2;
-            params.fs           = 1000; % sample rate (Hz)
-            params.shift        = 0;    % shift of onset response (in ms?)
-            params.scale        = 1;    % scale factor of final neural response
+            p.num_channels = 2;
+            p.fs           = 1000; % sample rate (Hz)
+            p.shift        = 0;    % shift of onset response (in ms?)
+            p.scale        = 1;    % scale factor of final neural response
           
         case '3ch-stLN' % load linear-nonlinear 3-channel model, with a sustained, transient-odd, transient-even channel
-            params.num_channels = 3;
-            params.fs           = 1000; % sample rate (Hz)
-            params.tau_s        = 4.93; % time constant for excitatory mechanism (ms) to create sustained gamma IRF
-            params.n1           = 9;    % number of stages in excitatory mechanism
-            params.n2           = 10;   % number of stages in inhibitory mechanism
-            params.kappa        = 1.33; % ratio of time constants for primary/secondary filters
-            params.exp          = 0.5;  % nonlinear compressive exponent for spatiotemporal nonlinearity
-
+            p.num_channels = 3;
+            p.fs           = 1000; % sample rate (Hz)
+            p.tau_s        = 4.93; % time constant for excitatory mechanism (ms) to create sustained gamma IRF
+            p.n1           = 9;    % number of stages in excitatory mechanism
+            p.n2           = 10;   % number of stages in inhibitory mechanism
+            p.kappa        = 1.33; % ratio of time constants for primary/secondary filters
+            if isfield(params.analysis.temporal.param,'exponent')
+                p.exp = params.analysis.temporal.param.exponent;
+            else % go with default
+                p.exp          = 0.5;  % nonlinear compressive exponent for spatiotemporal nonlinearity
+            end
     end
     
     % Pass values to params
     params.analysis.temporal.model        = params.analysis.temporalModel;
-    params.analysis.temporal.fs           = params.fs;
-    params.analysis.temporal.param        = params;
-    params.analysis.temporal.num_channels = params.num_channels;
+    params.analysis.temporal.fs           = p.fs;
+    params.analysis.temporal.param        = p;
+    params.analysis.temporal.num_channels = p.num_channels;
     params.analysis.temporal.tr           = tr;
 end
 
