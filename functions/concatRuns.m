ec0702256cb4243089f41559df2cd9538302900a
kiminsub <insekim@gmail.com>
2022-02-05 15:09:01 -0800

    added an option to also compute for 3d matrix when needed

--- functions/concatRuns.m
+++ functions/concatRuns.m
@@ -1,7 +1,18 @@
 function output = concatRuns(input)
 
-ordered_input = permute(input, [1 4 2 3]);
-output = reshape(ordered_input,[], size(input,2),size(input,3));  
+% input case 1) measurement * Voxel * channel * runs
+% input case 2) measurement * Voxel * runs
+% in either input cases, removes the last dimension (runs) and concats it
+% to the measurement dimension
+%% removes the last dimension and puts it back to 
+if ndims(input) == 4
+    ordered_input = permute(input, [1 4 2 3]);
+    output = reshape(ordered_input,[], size(input,2),size(input,3));
+elseif ndims(input) == 3
+    ordered_input = permute(input, [1 3 2]);
+    output = reshape(ordered_input,[], size(input,2));
+end
+
 
 %     output = reshape(input, ...
 %         [size(input,1)*size(input,4), size(input,2), size(input,3)]);
