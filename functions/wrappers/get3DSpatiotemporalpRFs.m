82beb6a791ebd7a4b35209926f47c20e532bd6fe
Eline Kupers <elinekupers@gmail.com>
2024-12-10 13:32:04 -0600

    Revert "reverting back to the previous version - updated code is not compatible"
    This reverts commit 22c53f2c034d6b67d2673e44835b4f727e8e45a2.

--- functions/wrappers/get3DSpatiotemporalpRFs.m
+++ functions/wrappers/get3DSpatiotemporalpRFs.m
@@ -26,55 +26,20 @@ switch params.analysis.temporalModel
         if ~isfield(x,'tau_t')
            x.tau_t = x.tau_s;              
         end
-
-                % Create temporal IRFs for sustained and transient channel
-        irfSustained = tch_irfs('S', x.tau_s, x.n1, x.n2, x.kappa, x.fs);
-        irfTransient = tch_irfs('T', x.tau_t, x.n1, x.n2, x.kappa, x.fs);
-        
-        f.temporal = zeros(max([length(irfSustained),length(irfTransient)]),3);
-        
-        % Normalize such that area under the curve sums to 1
-        f.temporal(1:length(irfSustained),1) = normSum(irfSustained);
-        
-        % For transient nrf, we do this separately for positive and negative parts:
-        % First find indices
-        pos_idx = irfTransient>=0;
-        neg_idx = irfTransient<0;
-        
-%         scf = 1;% 0.5; % sum of area under each pos/neg curve
-        scf = 0.5; % sum of area under each pos/neg curve
-        % Get positive part and normalize sum
-        irfT_pos = irfTransient(pos_idx);
-        irfT_pos = scf*normSum(irfT_pos);
-        
-        % Get negative part and normalize sum
-        irfT_neg = abs(irfTransient(neg_idx));
-        irfT_neg = -scf*normSum(irfT_neg);
-        
-        % Combine positive and negative parts
-        nrfT2 = NaN(size(irfTransient));
-        nrfT2(pos_idx) = irfT_pos;
-        nrfT2(neg_idx) = irfT_neg;
-        
-        f.temporal(1:length(nrfT2),2) = nrfT2;
-        f.temporal(1:length(nrfT2),3) = -nrfT2;
-        f.scaleFactorNormSumTransChan = scf;
-        clear nrfT2 irfT_pos irfT_neg
-
-        % f.temporal = zeros(5000,3,size(prfs,2));
-        % for ii = 1:numel(x.tau_s) %size(prfs,2)
-        %     x_n = struct('exponent',x.exponent(ii), ...
-        %                 'tau_s',x.tau_s(ii), ...
-        %                 'tau_t',x.tau_t(ii), ...
-        %                 'n1',x.n1, ...
-        %                 'n2',x.n2, ...
-        %                 'kappa', x.kappa, ...
-        %                 'fs', x.fs); 
-        % 
-        %     f_n = createTemporalIRFs_sustained_transient(x_n);
-        %     f.temporal(1:size(f_n.temporal,1),:,ii) = f_n.temporal;
-        %     if ii == 1; f.scaleFactorNormSumTransChan = f_n.scaleFactorNormSumTransChan; end
-        % end
+        f.temporal = NaN(5000,3,size(prfs,2));
+        for ii = 1:size(prfs,2)
+            x_n = struct('exponent',x.exponent(ii), ...
+                        'tau_s',x.tau_s(ii), ...
+                        'tau_t',x.tau_t(ii), ...
+                        'n1',x.n1, ...
+                        'n2',x.n2, ...
+                        'kappa', x.kappa, ...
+                        'fs', x.fs); 
+            
+            f_n = createTemporalIRFs_sustained_transient(x_n);
+            f.temporal(1:size(f_n.temporal,1),:,ii) = f_n.temporal;
+            if ii == 1; f.scaleFactorNormSumTransChan = f_n.scaleFactorNormSumTransChan; end
+        end
         f.names = {'sustained','transient_odd','transient_even'};
         % Get nr of filters
 %         nfilters = length(f.names);
